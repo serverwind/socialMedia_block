82a988812f11d7f109fcaa9e533221d1a33f43ee
Lestrangeqq <lestrangeqq@gmail.com>
2022-10-31 14:53:42 +0200

    Function to close block now stores data in local storage, so block will show if button to close never been clicked; if it's clicked block will position himself as the tiny line at the bottom of the page. Added 'mouseover' event, to show block again

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-let socButton = document.getElementById('closeSocial');
-let socBlock = document.querySelector('.socialBlock');
+var socButton = document.getElementById('closeSocial');
+var socBlock = document.querySelector('.socialBlock');
 
 socButton.addEventListener('click', closeSoc);
 
-function closeSoc() {
-    socBlock.style.display = 'none';
+if (localStorage.getItem('socialBlock', 'closed')) {
+    socBlock.style.bottom = '-55px';
+
+    var showSoc = function() {
+        socBlock.style.bottom = '0px';
+    };
+    socBlock.addEventListener('mouseover', showSoc);
 }
+
+function closeSoc() {
+    socBlock.style.bottom = '-55px';
+    localStorage.setItem("socialBlock", "closed");
+}
+
